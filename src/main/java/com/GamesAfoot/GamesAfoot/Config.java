43a161af3f8401b734b909df1fa4bf41f7bd0892
Jaime Mitchell <JaimeMitchell100@gmail.com>
2024-07-25 17:59:11 -0400

    add ai

---
+++ src/main/java/com/GamesAfoot/GamesAfoot/Config.java
@@ -0,0 +1,15 @@
+package com.GamesAfoot.GamesAfoot;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class Config {
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
+}
